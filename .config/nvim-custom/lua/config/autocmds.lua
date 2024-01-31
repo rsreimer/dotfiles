8d04e615b1a62f2f29e3c0b8d3828bae78acacd0
Rasmus Reimer <rasmus.reimer@gmail.com>
2024-01-31 22:22:03 +0100

    .

---
+++ .config/nvim-custom/lua/config/autocmds.lua
@@ -0,0 +1,15 @@
+-- Check if we need to reload the file when it changed
+vim.api.nvim_create_autocmd({ "FocusGained" }, {
+	callback = function()
+		if vim.o.buftype ~= "nofile" then
+			vim.cmd("checktime")
+		end
+	end,
+})
+
+-- Highlight on yank
+vim.api.nvim_create_autocmd("TextYankPost", {
+	callback = function()
+		vim.highlight.on_yank()
+	end,
+})
