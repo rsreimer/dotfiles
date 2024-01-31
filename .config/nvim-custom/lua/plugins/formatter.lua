9b21bc81bcd06a19e32c1d4d1e238e092748c0af
Rasmus Reimer <rasmus.reimer@gmail.com>
2024-01-31 20:34:25 +0100

    .

--- .config/nvim-custom/lua/plugins/formatter.lua
+++ .config/nvim-custom/lua/plugins/formatter.lua
@@ -25,9 +25,7 @@ return {
 			},
 		})
 
-		-- Format on save
 		vim.api.nvim_create_autocmd("BufWritePre", {
-			pattern = "*",
 			callback = function(args)
 				require("conform").format({
 					bufnr = args.buf,
