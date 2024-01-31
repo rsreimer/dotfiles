2f5f7327f926ece54dc52b311ec9a848b89283a7
Rasmus Reimer <rasmus.reimer@gmail.com>
2024-01-31 22:58:28 +0100

    .

--- .config/nvim-custom/init.lua
+++ .config/nvim-custom/init.lua
@@ -8,6 +8,6 @@ require("config.autocmds")
 require("config.keymaps")
 
 -- TODO: Mini.ai / tree sitter text objects
--- TODO: Comments: Plugins > Coding > mini.comment
+
 -- TODO: DAP
 -- TODO: Test Neotest
