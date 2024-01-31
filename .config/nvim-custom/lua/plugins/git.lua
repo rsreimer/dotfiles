9efafc27e6073fff237fb42f187b9775e326b7b7
Rasmus Reimer <rasmus.reimer@gmail.com>
2024-01-31 22:32:08 +0100

    .

--- .config/nvim-custom/lua/plugins/git.lua
+++ .config/nvim-custom/lua/plugins/git.lua
@@ -3,15 +3,15 @@ return {
 		"lewis6991/gitsigns.nvim",
 		config = function()
 			require("gitsigns").setup({
-				on_attach = function(buffer)
+				on_attach = function(bufnr)
 					local gs = package.loaded.gitsigns
 
-					vim.keymap.map("n", "]h", gs.next_hunk, { buffer = buffer, desc = "Next Hunk" })
-					vim.keymap.map("n", "[h", gs.prev_hunk, { buffer = buffer, desc = "Prev Hunk" })
+					vim.keymap.set("n", "]h", gs.next_hunk, { buffer = bufnr, desc = "Next Hunk" })
+					vim.keymap.set("n", "[h", gs.prev_hunk, { buffer = bufnr, desc = "Prev Hunk" })
 
-					vim.keymap.map("n", "<leader>gb", function()
+					vim.keymap.set("n", "<leader>gb", function()
 						gs.blame_line({ full = true })
-					end, { buffer = buffer, desc = "Blame Line" })
+					end, { buffer = bufnr, desc = "Blame Line" })
 				end,
 			})
 		end,
