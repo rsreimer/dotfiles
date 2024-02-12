403dac91d2513becf926eaf7558939f395686d63
Rasmus Reimer <rasmus.reimer@gmail.com>
2024-02-13 00:00:33 +0100

    .

---
+++ .config/nvim-custom/lua/plugins/colorscheme.lua
@@ -0,0 +1,20 @@
+return {
+	{
+		"folke/tokyonight.nvim",
+		enabled = false,
+		lazy = false,
+		priority = 1000,
+		config = function()
+			vim.cmd("colorscheme tokyonight")
+		end,
+	},
+	{
+		"rebelot/kanagawa.nvim",
+		enabled = true,
+		lazy = false,
+		priority = 1000,
+		config = function()
+			vim.cmd("colorscheme kanagawa")
+		end,
+	},
+}
