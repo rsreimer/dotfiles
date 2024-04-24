63cd228fc125cd8d7eccc3e4993f1f9c79713a7a
Rasmus Reimer <rasmus.reimer@gmail.com>
2024-04-24 13:51:30 +0200

    .

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -9,7 +9,6 @@ require("config.keymaps")
 
 -- TODO: DAP
 -- TODO: Neotest / jest.nvim?
--- TODO: Harpoon with bufferline https://gist.github.com/sashalikesplanes/dcbb4591deb96d704b7c37153182af1e
 -- TODO: setup trouble
 -- TODO: Typescript tools? https://github.com/pmizio/typescript-tools.nvim
 -- TODO: Git signs bindings for toggle blame, diff
--- .config/nvim/lua/plugins/which-key.lua
+++ .config/nvim/lua/plugins/which-key.lua
@@ -12,6 +12,7 @@ return {
 			["<leader>s"] = { name = "+search" },
 			["<leader>u"] = { name = "+ui" },
 			["<leader>x"] = { name = "+diagnostics/quickfix" },
+			["<leader>h"] = { name = "+harpoon" },
 		})
 	end,
 }
