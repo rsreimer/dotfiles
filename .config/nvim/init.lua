ee802719955acdaf93e8347cbfd95dc32244c0b7
Rasmus Reimer <rasmus.reimer@gmail.com>
2024-04-24 15:53:49 +0200

    .

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -7,8 +7,8 @@ require("utils.lazy").setup()
 require("config.autocmds")
 require("config.keymaps")
 
--- TODO: DAP
 -- TODO: Neotest / jest.nvim?
 -- TODO: setup trouble
+-- TODO: DAP
 -- TODO: Typescript tools? https://github.com/pmizio/typescript-tools.nvim
 -- TODO: Git signs bindings for toggle blame, diff
