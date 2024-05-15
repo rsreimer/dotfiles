161845355600a567d45e1721a10150cc633b7fe2
Rasmus Reimer <rasmus.reimer@gmail.com>
2024-05-15 21:19:14 +0200

    .

--- .config/nvim/lua/plugins/nvim-cmp.lua
+++ .config/nvim/lua/plugins/nvim-cmp.lua
@@ -23,7 +23,17 @@ return {
 				["<C-d>"] = cmp.mapping.scroll_docs(4),
 				["<C-Space>"] = cmp.mapping.complete(),
 				["<C-e>"] = cmp.mapping.abort(),
-				["<CR>"] = cmp.mapping.confirm({ select = true }),
+				["<Tab>"] = cmp.mapping(function(fallback)
+					if cmp.visible() then
+						local entry = cmp.get_selected_entry()
+						if not entry then
+							cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
+						end
+						cmp.confirm()
+					else
+						fallback()
+					end
+				end, { "i", "s", "c" }),
 			}),
 			sources = cmp.config.sources({
 				{ name = "nvim_lsp" },
