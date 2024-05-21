97f6d485b1802a58f3ff46bd0a203a86546448cd
Rasmus Reimer <rasmus.reimer@gmail.com>
2024-05-21 15:31:35 +0200

    .

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -48,6 +48,7 @@ opt.virtualedit = "block" -- Allow cursor to move where there is no text in visu
 opt.wildmode = "longest:full,full" -- Command-line completion mode
 opt.winminwidth = 5 -- Minimum window width
 opt.wrap = false -- Disable line wrap
+opt.smoothscroll = true
 opt.fillchars = {
   foldopen = "",
   foldclose = "",
@@ -57,7 +58,3 @@ opt.fillchars = {
   diff = "╱",
   eob = " ",
 }
-
-if vim.fn.has("nvim-0.10") == 1 then
-  opt.smoothscroll = true
-end
