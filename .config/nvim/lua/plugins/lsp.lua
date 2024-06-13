ca626792bead0bfd5e40f42807deca2c0ad432ce
Rasmus Reimer <rasmus.reimer@gmail.com>
2024-06-13 16:31:34 +0200

    vtsls

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -15,7 +15,7 @@ return {
         "cssls",
         "html",
         "jsonls",
-        "tsserver",
+        "vtsls",
         "lua_ls",
         "yamlls",
         "emmet_language_server",
@@ -47,6 +47,48 @@ return {
           root_dir = require("lspconfig.util").root_pattern("angular.json", "project.json"),
         })
       end,
+      ["vtsls"] = function()
+        require("lspconfig").vtsls.setup({
+          capabilities = cmp,
+          settings = {
+            complete_function_calls = true,
+            vtsls = {
+              enableMoveToFileCodeAction = true,
+              autoUseWorkspaceTsdk = true,
+              experimental = {
+                completion = {
+                  enableServerSideFuzzyMatch = true,
+                },
+              },
+            },
+            typescript = {
+              updateImportsOnFileMove = { enabled = "always" },
+              suggest = {
+                completeFunctionCalls = true,
+              },
+              inlayHints = {
+                enumMemberValues = { enabled = true },
+                functionLikeReturnTypes = { enabled = true },
+                parameterNames = { enabled = "literals" },
+                parameterTypes = { enabled = true },
+                propertyDeclarationTypes = { enabled = true },
+                variableTypes = { enabled = false },
+              },
+            },
+          },
+        })
+      end,
+    })
+
+    --HACK: disable angular renaming capability due to duplicate rename popping up from tsserver
+    vim.api.nvim_create_autocmd("LspAttach", {
+      callback = function(event)
+        local client = vim.lsp.get_client_by_id(event.data.client_id)
+
+        if client and client.name == "angularls" then
+          client.server_capabilities.renameProvider = false
+        end
+      end,
     })
 
     vim.api.nvim_create_autocmd("LspAttach", {
@@ -65,24 +107,13 @@ return {
         bmap("n", "gr", vim.lsp.buf.references, "References")
         bmap("n", "gl", vim.diagnostic.open_float, "Open Float")
 
+        -- stylua: ignore start
         bmap("n", "<leader>cr", vim.lsp.buf.rename, "Rename")
         bmap({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, "Code Actions")
-        bmap("n", "<leader>ci", function()
-          vim.lsp.buf.code_action({
-            apply = true,
-            context = {
-              only = { "source.addMissingImports.ts" },
-            },
-          })
-        end, "Add Missing Imports")
-        bmap("n", "<leader>cu", function()
-          vim.lsp.buf.code_action({
-            apply = true,
-            context = {
-              only = { "source.removeUnusedImports.ts" },
-            },
-          })
-        end, "Removed Unused Imports")
+        bmap("n", "<leader>ci", function() vim.lsp.buf.code_action({ apply = true, context = { only = { "source.addMissingImports.ts" } } }) end, "Add Missing Imports")
+        bmap("n", "<leader>cu", function() vim.lsp.buf.code_action({ apply = true, context = { only = { "source.removeUnusedImports.ts" } } }) end, "Removed Unused Imports")
+        bmap("n", "<leader>cD", function() vim.lsp.buf.code_action({ apply = true, context = { only = { "source.fixAll.ts" } } }) end, "Fix all diagnostics")
+        -- stylua: ignore end
 
         bmap("n", "[d", vim.diagnostic.goto_prev, "Prev Diagnostic")
         bmap("n", "]d", vim.diagnostic.goto_next, "Next Diagnostic")
