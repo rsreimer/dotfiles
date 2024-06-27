836f71a8fdfffbdd807f5c772a8faf5fc4ae8984
Rasmus Reimer <rasmus.reimer@gmail.com>
2024-06-27 09:09:05 +0200

    .

--- .config/nvim/lua/plugins/vimtex.lua
+++ .config/nvim/lua/plugins/vimtex.lua
@@ -1,9 +1,18 @@
 return {
   "lervag/vimtex",
   lazy = false, -- lazy-loading will disable inverse search
-  config = function()
+  init = function()
+    vim.g.vimtex_view_method = "skim"
     vim.g.vimtex_mappings_disable = { ["n"] = { "K" } } -- disable `K` as it conflicts with LSP hover
     vim.g.vimtex_quickfix_method = vim.fn.executable("pplatex") == 1 and "pplatex" or "latexlog"
+    vim.g.vimtex_compiler_latexmk = {
+      out_dir = "dist",
+      options = {
+        "-pdflatex=lualatex",
+        "-shell-escape",
+        "-pvc",
+      },
+    }
   end,
   keys = {
     { "<localLeader>l", "", desc = "+vimtext" },
